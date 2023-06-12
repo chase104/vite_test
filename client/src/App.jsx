651106b13b7ca1c8b1cccde9c29b4d21de3443e8
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-12 17:55:15 -0500

    add .env file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios('http://localhost:4001/extra_data').then((res) => {
+      console.log(res.data);
+    })
+  }, []);
 
   return (
     <>
@@ -26,7 +33,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the ICON for $800
       </p>
     </>
   )
--- vite_server.js
+++ vite_server.js
@@ -1,25 +1,26 @@
 // Require modules
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
+require('dotenv').config()
 // Create the Express app
 const app = express();
+
+app.use(cors({origin: "*"}))
 
 // Configure the app (app.set)
 app.use(express.static(path.join(__dirname, 'client/dist')));
 // app.use(express.static(path.join(__dirname, 'my_vite_app/dist')));
-
-
 // Mount middleware (app.use)
 
 
-// Mount routes
-app.get('/', function(req, res) {
-    res.set('Content-Type', 'text/html')
-  	res.json({data: "I am a string"});
-});
 
+app.get('/extra_data', (req, res) => {
+  console.log(process.env.SECRET)
+  res.send(process.env.SECRET);
+
+})
 // Tell the app to listen on port 3000
-app.listen(3000, function() {
- console.log('Listening on port 3000');
+app.listen(4001, function() {
+ console.log('Listening on port 4001');
 });
